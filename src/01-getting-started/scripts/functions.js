1ebbd9d1c5cb0e9ef9013babf387bff4ade0eee4
New Pitsini <new.pitsini@gmail.com>
2019-10-07 16:05:02 -0600

    finished Exercise-Enhancement (Comp100)

--- src/01-getting-started/scripts/functions.js
+++ src/01-getting-started/scripts/functions.js
@@ -2,8 +2,10 @@
 const functions = {
     
     size: (num) => {
+        if (num < 0) return "Negative";
         if (num < 10) return "small";
         if (num < 20) return "medium";
+        if (num > 100) return "extra large";
         return "large";
     },
 
