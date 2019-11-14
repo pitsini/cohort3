e42fdcb02362cce3e54b818a7b68e6e1a4502f8d
New Pitsini <new.pitsini@gmail.com>
2019-11-14 16:05:32 -0700

    working on 140A - learning React

--- react-01/src/App.js
+++ react-01/src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { MyComponent, EvenComponent, OddComponent} from './components/MyComponent';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super();
+    this.counter = 21;
+    this.state = {
+      myState: "TBD",
+
+    }
+  }
+
+  onPushMe = (event) => {
+    console.log("You pushed me");
+    this.counter ++;
+    this.setState({
+      myState: "now:" + this.counter
+    });
+  }
+
+  EvenOrOdd = (props) => {
+    if (props.countNumber % 2 === 0) { //even
+    return <EvenComponent />;
+  }
+    return <OddComponent />; //odd
+}
+
+  render() {
+    const whatToSay = "What Ever";
+    
+    return (
+      
+      <div className="App">
+        <header className="App-header">
+          <this.EvenOrOdd countNumber={this.counter}/>
+          <MyComponent sayWhat={whatToSay} pushChange={this.onPushMe} />
+          {/* <MyComponent sayWhat={whatToSay} pushChange={this.onPushMe} /> */}
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>I am in control of this application and my name is New {this.state.myState}</h1>
+
+          if Even
+          <EvenComponent />
+          else 
+
+          <OddComponent />
+          {/* <button onClick={this.onPushMe}>
+            Push Me
+          </button> */}
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
